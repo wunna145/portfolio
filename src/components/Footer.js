af4785a9a4e7925aa90150bb408ef74350d531e2
Zaw Wunna <zawwunna@Zaws-Air.lan>
2024-06-15 00:09:43 -0700

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,11 @@ import './Footer.css'
 const Footer = () => {
   return (
     <footer className='footer'>
-      <p className='footer-text'>Developed using <span class="highlight">Visual Studio Code</span>.
-      This site is built with <span class="highlight">React.js</span> and styled 
-      with <span class="highlight">CSS</span>. Images are dynamically imported and 
-      displayed based on project data. Deployed seamlessly on <span class="highlight">Render</span>.
+      <p className='footer-text'>Developed by <span class="highlight">Zaw Wunna </span> 
+      using <span class="highlight">Visual Studio Code</span>, this site is built with 
+      <span class="highlight"> React.js</span> and styled with <span class="highlight">CSS</span>. 
+      Images are dynamically imported and displayed based on project data. 
+      Deployed seamlessly on <span class="highlight">Render</span>.
       </p>
     </footer>
   );
